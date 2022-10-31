ebfb46d500713aa1e3708142c33fe2a2f3014a43
Hanbi58 <hanxiaonan2012@gmail.com>
2022-10-31 16:55:55 -0700

    animation

--- src/comtainers/LandingImage/LandingImage.js
+++ src/comtainers/LandingImage/LandingImage.js
@@ -1,18 +1,41 @@
 import classes from "./landingImage.module.css";
+import { useState } from "react";
 
 function LandingImage() {
+  const [slowDown, setSlowDown] = useState(false);
+  const mouseEnterHandler = () => {
+    setSlowDown(true);
+  };
+  const mouseLeaveHandler = () => {
+    setSlowDown(false);
+  };
+  const cloudMidCls = slowDown
+    ? `${classes.cloudMid} ${classes.slowdown}`
+    : `${classes.cloudMid}`;
+  const cloudTopCls = slowDown
+    ? `${classes.cloudTop} ${classes.slowdown}`
+    : `${classes.cloudTop}`;
+  const cloudBottomCls = slowDown
+    ? `${classes.cloudBottom} ${classes.slowdown}`
+    : `${classes.cloudBottom}`;
   return (
     <div className={classes.container}>
       <div className={classes.mountainMain}></div>
-      <div className={classes.cloudTop}></div>
-      <div className={classes.cloudMid}></div>
+      <div className={cloudTopCls}></div>
+      <div className={cloudMidCls}></div>
       <div className={classes.mountainLeft}></div>
       <div className={classes.mountainRight}></div>
-      <div className={classes.cloudBottom}></div>
+      <div className={cloudBottomCls}></div>
 
-      <p className={classes.titleFirst}>TOP PEAKS</p>
+      <p
+        className={classes.titleFirst}
+        onMouseEnter={mouseEnterHandler}
+        onMouseLeave={mouseLeaveHandler}
+      >
+        TOP PEAKS
+      </p>
 
-      <p className={classes.explore}>Explore Roof of the world.</p>
+      <p className={classes.explore}>Explore roof of the world.</p>
     </div>
   );
 }
