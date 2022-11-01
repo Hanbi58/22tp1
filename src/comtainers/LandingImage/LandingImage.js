ec283753f229cd2d3785b77695766c7b5cc91a6a
Hanbi58 <hanxiaonan2012@gmail.com>
2022-10-31 22:41:35 -0700

    motion

--- src/comtainers/LandingImage/LandingImage.js
+++ src/comtainers/LandingImage/LandingImage.js
@@ -41,3 +41,27 @@ function LandingImage() {
 }
 
 export default LandingImage;
+
+var findPeakElement = function (nums) {
+  if (nums.length === 1) {
+    return 0;
+  }
+  if (nums[0] > nums[1]) {
+    return 0;
+  }
+  if (nums[nums.length - 1] > nums[nums.length - 2]) {
+    return nums.length - 1;
+  }
+  let left = 1;
+  let right = nums.length - 2;
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (nums[mid - 1] < nums[mid] && nums[mid] > nums[mid + 1]) {
+      return mid;
+    } else if (nums[mid + 1] > nums[mid]) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+};
