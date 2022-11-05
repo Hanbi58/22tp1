315f4a86aadbda89852528df2d43bc8cd4aec2ba
Hanbi58 <hanxiaonan2012@gmail.com>
2022-11-04 22:18:37 -0700

    Swiper

--- src/comtainers/list/List.js
+++ src/comtainers/list/List.js
@@ -1,7 +1,27 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 import { listContent } from "../../assets/listContent/ListContent";
 import classes from "./list.module.css";
+import { ListImage } from "../../components";
+
+const imageSwipers = listContent.map((listContent) => (
+  <SwiperSlide>
+    <ListImage listContent={listContent} />
+  </SwiperSlide>
+));
 function List() {
-  return <main className={classes.listContainer} id="rank"></main>;
+  return (
+    <main className={classes.listContainer} id="rank">
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={3}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        {imageSwipers}
+      </Swiper>
+    </main>
+  );
 }
 
 export default List;
