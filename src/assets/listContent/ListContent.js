99d7b0172b75da83896c4f4204bc5fd66a310e37
Hanbi58 <hanxiaonan2012@gmail.com>
2022-11-07 22:18:10 -0800

    WIP

--- src/assets/listContent/ListContent.js
+++ src/assets/listContent/ListContent.js
@@ -3,12 +3,22 @@ import twoB from "./02b7.jpg";
 import threeB from "./03b2.jpg";
 import fourB from "./04b6.jpg";
 import fivB from "./05b.jpg";
+import sixB from "./06b3.jpg";
+import sevenB from "./07b4.jpg";
+import eightB from "./08b3.jpg";
+import nineB from "./09b4.jpg";
+import tenB from "./10b4.jpg";
 
 import oneS from "./01s.jpg";
 import twoS from "./02s.jpg";
 import threeS from "./03s3.jpg";
 import fourS from "./04s.jpg";
 import fivS from "./05s.jpg";
+import sixS from "./06s.jpg";
+import sevenS from "./07s.jpg";
+import eightS from "./08s.jpg";
+import nineS from "./09s.jpg";
+import tenS from "./10s2.jpg";
 
 export const listContent = [
   {
@@ -80,5 +90,75 @@ export const listContent = [
     longDescription:
       "It was first summited by a French expedition led by Jean Franco in 1955. Their ascent was most notable for the fact that a full ten members of the expedition team summited the mountain on the trip. In those days, it was normally only one or two climbers from each team who reached the top of the mountain on an expedition, so this was a huge deal at the time, and generally, it's just quite nice, isn't it? The first two summited on 15 May 1955, then four more went up the next day, then four more went up the day after that. Just very wholesome mountain climbing, really.",
     smallImageUrl: fivS,
+  },
+  {
+    id: "06",
+    heightMeters: 8188,
+    heightFeet: 26864,
+    mountainName: "Cho Oyu",
+    range: "Himalayas",
+    region: "China/Nepal",
+    imageUrl: sixB,
+    shortDescription:
+      "Widely considered the most achievable of the world's fourteen 8,000-meter peaks. Frequently climbed both with and without supplemental oxygen.",
+    longDescription:
+      "It has a 63.4% success rate with nearly 4,000 climbers and guides having reached the summit to date, the highest number of all the eight-thousanders, with the exception of Mt. Everest.10 Climbers tend to use this mountain as a stepping stone to train for Everest or to see how their body reacts to the high altitude. That’s not to say scaling this massive mountain isn't dangerous, however; Cho Oyu has still claimed the lives of at least 52 people since 1952.",
+    smallImageUrl: sixS,
+  },
+  {
+    id: "07",
+    heightMeters: 8167,
+    heightFeet: 26795,
+    mountainName: "Dhaulagiri I",
+    range: "Himalayas",
+    region: "Nepal",
+    imageUrl: sevenB,
+    shortDescription:
+      "This snow-capped mountain in the west-central part of Nepal is the largest located entirely within the country. ",
+    longDescription:
+      "It is situated on the western side of the Kali Gandaki River gorge, believed to be the world'’'s deepest subaerial valley, consisting of several glacier-covered peaks that exceed 25,000 feet. There have been over 550 successful ascents of Dhaulagiri I, the highest peak at 26,795 feet, since 1953. Similar to Everest, the summit of Dhaulagiri is composed of limestone and dolomite rock layers that originally formed at the bottom of the ocean hundreds of millions of years ago and were pushed up by powerful tectonic forces.",
+    smallImageUrl: sevenS,
+  },
+  {
+    id: "08",
+    heightMeters: 8163,
+    heightFeet: 26781,
+    mountainName: "Manaslu",
+    range: "Himalayas",
+    region: "Nepal",
+    imageUrl: eightB,
+    shortDescription:
+      "One of the more dangerous of the eight-thousanders due to its high number of avalanches. Just over 52% of expeditions are successful and there's a death rate of 1 in 10 among climbers.",
+    longDescription:
+      "In 1974, an all-female team from Japan became the first women to successfully summit an 8,000-meter peak when they reached the top of Manaslu, which measures 26,781 feet.12 The 642-square-mile Manaslu Conservation Area was declared in 1998 to protect the habitats of the 33 mammal species, 110 bird species, 11 butterfly species, and three reptile species that live in the Manaslu region in the North Nepalese Himalayas.",
+    smallImageUrl: eightS,
+  },
+  {
+    id: "09",
+    heightMeters: 8126,
+    heightFeet: 26660,
+    mountainName: "Nanga Parbat",
+    range: "Himalayas",
+    region: "Pakistan",
+    imageUrl: nineB,
+    shortDescription:
+      "Nanga Parbat has tremendous vertical relief over local terrain in all directions. Earned the nickname Killer Mountain for its high number of climber fatalities.",
+    longDescription:
+      "Today, the 26,660-foot mountain in Pakistan has seen at least 339 successful summits and 69 deaths, giving it a death rate more than six times that of Everest.13 Nanga Parbat attracts geologists as well since it’s rising at a rate of 7 millimeters (0.275 inches) per year, making it the fastest rising mountain on Earth.14 Scientists believe this is due to erosion, which reduces the weight of the mountain range and accelerates the tectonic process below the mountain.",
+    smallImageUrl: nineS,
+  },
+  {
+    id: "10",
+    heightMeters: 8091,
+    heightFeet: 26545,
+    mountainName: "	Annapurna I",
+    range: "Himalayas",
+    region: "Nepal",
+    imageUrl: tenB,
+    shortDescription:
+      "Although its 26,545 feet make it just the tenth tallest on the list, it has the highest fatality to summit ratio (38%).",
+    longDescription:
+      "Annapurna I was the first 8,000-metre (26,200 ft) peak to be climbed.  On the other side of Dhaulagiri, across the Kali River gorge in Nepal, Annapurna is perhaps the most deadly mountain in the world. In 1950, Maurice Herzog and Louis Lachenal were the first to reach the summit (losing their toes and fingers to frostbite as a result), marking the first of the Earth's 14 eight-thousanders to be scaled. A documentary of the expedition was made, called Victoire sur l'Annapurna. Its summit was the highest summit attained for three years, until the first successful ascent of Mount Everest had already been attained on Everest in the 1920s. Another successful climb wasn't achieved until 20 years later.",
+    smallImageUrl: tenS,
   },
 ];
