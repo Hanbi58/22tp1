0891804b8abc026730eb1d457cc94a077341b41e
Hanbi58 <hanxiaonan2012@gmail.com>
2022-11-15 19:17:55 -0800

    WIP

--- src/components/Carousal/Carousal.js
+++ src/components/Carousal/Carousal.js
@@ -27,7 +27,7 @@ function Carousal({ listContentItem, isOpen, openHandler }) {
 
       <motion.div
         className={tabClass}
-        drag
+        drag="y"
         dragSnapToOrigin={true}
         dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
         dragElastic={0.1}
